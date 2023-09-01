19840ae0166cade0b0e87ac3c9fb1bf9daeb8c58
Mohamed-Ahmed-Kamel <MohamedAhmedKamel159@gmail.com>
2023-09-01 15:21:38 +0300

    v1

---
+++ README.md
@@ -0,0 +1,9 @@
+Sure! Here's a README file for the code you provided:
+
+# Weather App
+
+This is a simple weather app that allows users to search for the weather in a specific city. The app displays the current temperature, humidity, and wind speed for the selected city.
+
+## Usage
+
+To use the app, simply type the name of the city you want to search for in the search bar. The weather data for that city will be displayed in the weather section of the app.
---
+++ app.js
@@ -0,0 +1,41 @@
+const api_key = "";
+const api_url = `https://api.openweathermap.org/data/2.5/weather?units=metric&q=`;
+
+// const search_input = document.querySelector(".search input");
+// const search_btn = document.querySelector(".search button");
+
+const get_weather_data = async (city) => {
+  await fetch(api_url + city + `&appid=${api_key}`)
+    .then((result) => result.json())
+    .then((json) => {
+      document.querySelector(".temp").innerHTML =
+        Math.round(json.main.temp) + "°C";
+      document.querySelector(".city").innerHTML = json.name;
+      document.querySelector(".humidity").innerHTML = json.main.humidity + "%";
+      document.querySelector(".wind").innerHTML = json.wind.speed + " km/h";
+
+      // console.log(json);
+      if (json.weather[0].main == "Rain")
+        document.querySelector(".weather .weather-icon i").className =
+          "fa-solid fa-cloud-showers-heavy";
+      if (json.weather[0].main == "Clear")
+        document.querySelector(".weather .weather-icon i").className =
+          "fa-solid fa-sun";
+      if (json.weather[0].main == "Clouds")
+        document.querySelector(".weather .weather-icon i").className =
+          "fa-solid fa-cloud";
+      if (json.weather[0].main == "Drizzle")
+        document.querySelector(".weather .weather-icon i").className =
+          "fa-solid fa-cloud-sun-rain";
+      if (json.weather[0].main == "Mist")
+        document.querySelector(".weather .weather-icon i").className =
+          "fa-solid fa-cloud-sun";
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+// search_btn.addEventListener("click", () => {
+//     get_weather_data(search_input.value)
+// });
+get_weather_data("ciro");
